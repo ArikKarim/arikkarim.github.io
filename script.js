46b1f4c91b3e8fbef2f2a0ef64695340fe590257
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2024-12-25 18:00:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const toggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+});
